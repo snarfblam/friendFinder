ba2895b0db955421695c17cce467b79e7480f684
Tom H <snarfblam@gmail.com>
2018-03-13 20:21:24 -0400

    added GET /api/friends to apiRoutes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+var friends = require('../data/friends');
+
+var routes = [
+    {
+        path: "/api/friends",
+        type: "GET",
+        func: function (req, res) {
+            res.writeHead(200, { "Content-Type": "text/json" });
+            res.end(JSON.stringify(friends.getFriends));
+        }
+    }
+];
