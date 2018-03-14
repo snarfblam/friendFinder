c91151693ae73a5b062f9fd9d5ebfcc3af2b4201
Tom H <snarfblam@gmail.com>
2018-03-13 21:55:40 -0400

    Added html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var path = require('path');
+
+var routes = [
+    {
+        path: "/survey",
+        method: "GET",
+        func: function (req, res) {
+            sendFile(res, 'survey.html');
+            // res.sendFile(path.join(process.env.htmlRoot, 'survey.html'));
+        }
+    },
+    {
+        path: "/*",
+        method: "GET",
+        func: function (req, res) {
+            sendFile(res, 'home.html');
+        }
+    },
+
+];
+
+/** Returns a boolean indicating whether or not the file was sent. */
+function sendFile(response, relativePath) {
+    var absPath = path.join(process.env.htmlRoot, relativePath);
+    if (!fs.existsSync(absPath)) return false;
+    response.sendFile(absPath);
+    
+}
+
+module.exports = {
+    routes: routes,
+};
