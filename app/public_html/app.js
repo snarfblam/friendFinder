a6d01e021f1049285f936cee7a158eb600bcc4e0
Thomas Hudson <snarfblam@gmail.com>
2018-03-17 10:04:47 -0400

    Added some survey parsing code

---
+++ app/public_html/app.js
@@ -0,0 +1,53 @@
+function parseSurveyForm() {
+    var responses = getSurveyValues();
+    var name = $('#survey-name').val();
+    var photoUrl = $('#survey-photoUrl').val();
+
+    var formInvalid = !name || !photoUrl || responses.unanswered.length != 0;
+
+    if (!name) {
+        markTextFieldInvalid('#survey-name');
+    }
+    if (!photoUrl) {
+        markTextFieldInvalid('#survey-photoUrl');
+    }
+    responses.unanswered.forEach(index => {
+        markSurveyQuestionInvalid(index);
+    });
+}
+
+function markTextFieldInvalid(elementSelector) {
+    // Need to create css and apply class
+}
+
+function markSurveyQuestionInvalid(index) {
+    // need to create css and find/add ID to select on, and apply class
+}
+
+function getSurveyValues() {
+    var values = [];
+    var unansweredQuestions = [];
+    for (var i = 0; i < 10; i++) {
+        var singleResult = getQuestionResponse(i);
+        if (singleResult == null) {
+            unansweredQuestions.push(i);
+        } 
+        values.push(singleResult);
+    }
+
+    return {
+        values: values,
+        unanswered: unansweredQuestions,
+    };
+}
+
+function getQuestionResponse(questionNumber) {
+    var name = 'q' + questionNumber;
+    var selector = "input[name='q3']:checked";
+    var $input = $(input);
+    if ($input.length) {
+        return$input.val() || null;
+    } else {
+        return null;
+    }
+}
